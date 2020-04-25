3a68593f98f9d169c4290592bf5c8d986938b89e
Turan <tshukurbeyli@gmail.com>
2020-04-25 15:43:09 +0900

    configuring project

---
+++ ShopApp/data.js
@@ -0,0 +1,61 @@
+module.exports = function () {
+  return {
+    products: [
+      {
+        id: 1,
+        name: "Samsung S5",
+        price: "1000",
+        imageUrl: "1.jpg",
+        description: "1 kameralı",
+      },
+      {
+        id: 2,
+        name: "Samsung S6",
+        price: "2000",
+        imageUrl: "2.jpg",
+        description: "2 kameralı",
+      },
+      {
+        id: 3,
+        name: "Samsung S7",
+        price: "3000",
+        imageUrl: "3.jpg",
+        description: "3 kameralı",
+      },
+      {
+        id: 4,
+        name: "Samsung S8",
+        price: "4000",
+        imageUrl: "4.jpg",
+        description: "4 kameralı",
+      },
+      {
+        id: 5,
+        name: "Samsung S9",
+        price: "5000",
+        imageUrl: "5.jpg",
+        description: "5 kameralı",
+      },
+      {
+        id: 6,
+        name: "Samsung S10",
+        price: "6000",
+        imageUrl: "6.jpg",
+        description: "6 kameralı",
+      },
+      {
+        id: 8,
+        name: "Samsung S11",
+        price: "7000",
+        imageUrl: "7.jpg",
+        description: "7 kameralı",
+      },
+    ],
+    categories: [
+      { id: 1, name: "Telefon" },
+      { id: 2, name: "Komputer" },
+      { id: 3, name: "Elektronika" },
+    ],
+    orders: [],
+  };
+};
