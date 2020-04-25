1730ac87fbcbcedc9fec676edfe3dee93f54a417
Turan <tshukurbeyli@gmail.com>
2020-04-25 14:08:33 +0900

    save all data

---
+++ ShopApp/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "root",
+  template: '<div class="bg-primary m-5 p-5" >Shop Aplication</div>',
+})
+export class AppComponent {}
---
+++ mobileshop+backend/src/test/java/com/turanshukur/mobileshop/MobileshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turanshukur.mobileshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
